7e9e160c1121f680b36561801f69f1e5ba704cea
Radonirina Ranaivonjanahary <radonirina.ranaivonjanahary@netapsys.fr>
2018-03-05 11:18:11 +0300

    rm unsuded import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 
-import { ListModule } from './component/list/list.module';
-import { DetailModule } from './component/detail/detail.module';
-
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'survey', loadChildren: 'app/component/list/list.module#ListModule' },
